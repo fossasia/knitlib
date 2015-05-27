4b2758169aea11c339741264de6696a17684548f
Sebastian Oliva <code@sebastianoliva.com>
2015-05-27 01:27:15 -0600

    Added machine handler and PluginType enums

---
+++ src/knitlib/machine_handler.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import plugins
+
+class MachineHandler():
+  """Handles allocation of machines to plugins and resources.
+
+  Machine Plugins are defined as subclasses of BaseKnittingPlugin. Each Machine
+  plugin can be matched to a port or interface. Each port can only handle one
+  machine at once.
+
+  """
+
+  def get_machines_by_type(machine_type):
+    """Returns a list of the available plugins for a given PluginType or empty array if none found."""
+    return plugins.active_plugins.get(machine_type, [])
