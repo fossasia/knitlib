90aca8e73b4ec145f8be222bef880d18540aab9c
Sebastian Oliva <code@sebastianoliva.com>
2015-06-19 00:00:20 -0600

    fixed conf dict for machine knitting config.

--- src/knitlib/__main__.py
+++ src/knitlib/__main__.py
@@ -11,11 +11,11 @@ import knitlib
 @click.command()
 @click.option('--plugin_name', default="DummyKnittingPlugin",  # prompt='Name of the Machine Plugin you want.',
               help='The name of the Machine Plugin you want.')
-@click.option('--config', multiple=True, nargs=2, type=click.Tuple([unicode, int]))
+@click.option('--config', multiple=True, nargs=2, type=click.Tuple([unicode, unicode]))
 def main(plugin_name, config):
     plugin = knitlib.machine_handler.get_machine_plugin_by_id(plugin_name)
     machine_instance = plugin()
-    machine_instance.configure(config)
+    machine_instance.configure(dict(config))
     machine_instance.knit()
     machine_instance.finish()
 
