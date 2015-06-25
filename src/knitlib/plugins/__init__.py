cb2b86d447ac6b29e166ed95b47fd34f2e622470
Sebastian Oliva <code@sebastianoliva.com>
2015-06-25 07:50:43 -0600

    adding pdd to list of plugins, albeit it is not working yet

--- src/knitlib/plugins/__init__.py
+++ src/knitlib/plugins/__init__.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
+from enum import Enum
+
 import dummy_plugin
 import ayab_plugin
+import pdd_plugin
 
-from enum import Enum
 
 PluginType = Enum('PluginType', 'serial network other')
 '''PluginTypes holds an enumeration of the type of machine plugins available for use.'''
 
 active_plugins = {
-    PluginType.other: {dummy_plugin.DummyKnittingPlugin.__name__: dummy_plugin.DummyKnittingPlugin,
-                       ayab_plugin.AyabPluginControl.__name__: ayab_plugin.AyabPluginControl
-                       },
+    PluginType.other: {dummy_plugin.DummyKnittingPlugin.__name__: dummy_plugin.DummyKnittingPlugin},
+    PluginType.serial: {ayab_plugin.AyabPluginControl.__name__: ayab_plugin.AyabPluginControl,
+                        pdd_plugin.PDDEmulationKnittingPlugin.__name__: pdd_plugin.PDDEmulationKnittingPlugin,
+                        }
 }
