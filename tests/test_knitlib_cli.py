23afc68b95982d30c417d04b312430718971231f
Sebastian Oliva <code@sebastianoliva.com>
2015-06-27 13:49:48 -0600

    Renamed test_knitlib to test_knitlib_cli

--- tests/test_knitlib_cli.py
+++ tests/test_knitlib_cli.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 from click.testing import CliRunner
 import mock
-import knitlib.plugins.dummy_plugin
 from knitlib import __main__ as main
 
 
-
-def test_main():
+def test_main_init_with_no_args():
     runner = CliRunner()
     result = runner.invoke(main, [])
 
@@ -14,11 +12,19 @@ def test_main():
     assert result.exit_code is not 0  # It should fail due to invalid configuration,
 
 
+@mock.patch("knitlib.__main__.logging")
+def test_invalid_plugin_error(mock_logging):
+    runner = CliRunner()
+    result = runner.invoke(main, ["--plugin_name", "INVALID_PLUGIN"])
+    assert mock_logging.error.assert_called()
+    assert result.output is not '()\n'
+    assert result.exit_code is -1
+
+
 @mock.patch('knitlib.plugins.dummy_plugin.logging')
 def test_dummy_knit(mock_logging):
     runner = CliRunner()
-    result = runner.invoke(main, ["--plugin_name", "DummyKnittingPlugin"])
-
+    result = runner.invoke(main, ["--plugin_name", "dummy"])
     assert mock_logging.debug.assert_called()
 
 
