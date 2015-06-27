8760a88b7da9ad05d84c60cc95cf942e6a34ad19
Sebastian Oliva <code@sebastianoliva.com>
2015-06-27 14:26:28 -0600

    Added init launch for pdd plugin for demo.

--- src/knitlib/plugins/pdd_plugin/__init__.py
+++ src/knitlib/plugins/pdd_plugin/__init__.py
@@ -1,7 +1,9 @@
 __author__ = 'tian'
 
+import sys
 import logging
 import PDDemulate
+from PDDemulate import PDDemulator
 from knitlib.plugins.knitting_plugin import BaseKnittingPlugin
 
 
@@ -11,7 +13,13 @@ class PDDEmulationKnittingPlugin(BaseKnittingPlugin):
         logging.debug("Loaded PDD emulator version: {}".format(PDDemulate.version))
         # TODO: init emulator
 
+    def register_interactive_callbacks(self, callbacks=None):
+        super(PDDEmulationKnittingPlugin, self).register_interactive_callbacks(callbacks)
+
     def validate_configuration(self, conf):
+        pass
+
+    def publish_options(self):
         pass
 
     def onfinish(self, e):
@@ -22,3 +30,23 @@ class PDDEmulationKnittingPlugin(BaseKnittingPlugin):
 
     def onknit(self, e):
         pass
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print 'Usage: %s basedir serialdevice' % sys.argv[0]
+        sys.exit()
+
+    print 'Preparing . . . Please Wait'
+    emu = PDDemulator(sys.argv[1])
+
+    # TODO: fix usb port hardcoding
+    emu.open(cport=sys.argv[2])
+
+    print 'Emulator Ready!'
+    try:
+        while 1:
+            emu.handleRequests()
+    except (KeyboardInterrupt):
+        pass
+
+    emu.close()
