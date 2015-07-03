6ff13dd62321f19f060c45dd6ac55ded614f9d17
Sebastian Oliva <code@sebastianoliva.com>
2015-07-03 11:21:38 -0600

    Added validate method to knitpat submodule.

--- src/knitlib/knitpat/__init__.py
+++ src/knitlib/knitpat/__init__.py
@@ -1,5 +1,16 @@
 __author__ = 'tian'
 
+import validictory
+import json
+
+
+__SCHEMA_FILE = file("knitting_pattern_schema.json", "rb")
+__SCHEMA_DICT = json.load(__SCHEMA_FILE)
+
+
+def validate(loaded_json_data):
+    validictory.validate(loaded_json_data, __SCHEMA_DICT)
+
 
 def parse():
     pass
