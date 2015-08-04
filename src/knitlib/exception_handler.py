07d7994bc32149eadf503a51f90f67343605edce
shiluka <shiluka@gmail.com>
2015-08-05 01:27:30 +0530

    added basic functions for th exception handler

---
+++ src/knitlib/exception_handler.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# This file is part of Knitlib.
+#
+#    Knitlib is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Knitlib is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Knitlib.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Copyright 2015 Sebastian Oliva, Shiluka Dharmasena <http://github.com/fashiontec/knitlib>
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class InputError(Error):
+    """Exception raised for errors in the input.
+    
+    Attributes:
+        expression -- input expression in which
+                          the error occurred
+        message -- explanation of the error
+    """
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+
+class CommunicationException(Exception):
+    """Exception raised for errors in the input."""
+    pass
